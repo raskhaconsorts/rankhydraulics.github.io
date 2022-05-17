50b1cc235d77645e06761cabaf185e57cd770183
dentalacare <97787322+raskhaconsorts@users.noreply.github.com>
2022-05-17 15:34:55 +0530

    commt changes

--- rank.js
+++
@@ -1,8 +0,0 @@
-function menuShow() {
-	document.getElementById("menu").classList.toggle("show");
-	
-}
-function menuClose() {
-	document.getElementById("menu").classList.remove("show");
-}
-/******************* collapse dropdown closing ***************************/
